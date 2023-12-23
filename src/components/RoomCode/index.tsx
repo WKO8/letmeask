5de1b142bfa3e6d3cb60e4ce90abd3f1ec98bdae
W_K.O <wko.programmer@gmail.com>
2023-12-23 18:53:28 -0300

    Interaction with rooms - 0.3.0
    - Like question functionality
    - Administration room page
    - Remove question functionality
    - Close room functionality

--- src/components/RoomCode/index.tsx
+++ src/components/RoomCode/index.tsx
@@ -1,6 +1,6 @@
-import copyImg from '../assets/images/copy.svg';
+import copyImg from '../../assets/images/copy.svg';
 
-import '../styles/room-code.css';
+import './styles.scss';
 
 type RoomCodeProps = {
     code: string;
